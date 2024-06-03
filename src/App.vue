3b3dfd5553025a33a16c2d5ee1a901b1e0c14eb7
ghiottoandrea <andreaghiotto9@gmail.com>
2024-06-03 16:57:54 +0200

    Create ProjectCard and print in page

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,22 @@
 <script>
 import axios from 'axios';
+import ProjectCard from './components/ProjectCard.vue';
 
 export default {
   name: 'App',
+  components: {
+    ProjectCard
+  },
   data() {
     return {
-      base_api_url: 'http://127.0.0.1:8000',
-      base_projects_url: '/api/projects',
-      projects: []
     }
-  },
-  methods: {
-
-
-    callApi(url) {
-      axios
-        .get(url)
-        .then(response => {
-          console.log(response.data);
-          this.projects = response.data;
-        })
-    }
-  },
-  mounted() {
-    let url = this.base_api_url + this.base_projects_url;
-    this.callApi(url);
   }
 }
 </script>
 
 <template>
 
+  <ProjectCard />
 
 </template>
 
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,47 @@
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'ProjectCard',
+    data() {
+        return {
+            base_api_url: 'http://127.0.0.1:8000',
+            base_projects_url: '/api/projects',
+            projects: []
+        }
+    },
+    methods: {
+
+        callApi(url) {
+            axios
+                .get(url)
+                .then(response => {
+                    console.log(response.data.projects);
+                    this.projects = response.data.projects;
+                })
+        }
+    },
+    mounted() {
+        let url = this.base_api_url + this.base_projects_url;
+        this.callApi(url);
+    }
+}
+</script>
+
+<template>
+
+    <div>
+        <h1>Projects</h1>
+        <ul>
+            <li v-for="project in projects.data" :key="project.id">
+                <h2>{{ project.title }}</h2>
+                <p>{{ project.description }}</p>
+                <p>Start date: {{ project.project_start_date }}</p>
+                <p>End date: {{ project.project_end_date }}</p>
+            </li>
+        </ul>
+    </div>
+
+</template>
+
+<style></style>
